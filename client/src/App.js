f8109a2cdddd6c31b75e12df485bdacb219c032d
utkrishta24 <krishsinha24062gmail.com>
2023-11-05 15:53:42 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,65 @@
+import {
+  ChakraProvider,
+  Heading,
+  Container,
+  Text,
+  Input,
+  Button,
+  Wrap,
+  Stack, 
+  Image,
+  Link,
+  SkeletonCircle,
+  SkeletonText,
+} from "@chakra-ui/react";
+import axios from "axios";
+import { useState } from "react";
+
+const App = () => {
+  const [image, updateImage] = useState();
+  const [prompt, updatePrompt] = useState();
+  const [loading, updateLoading] = useState();
+
+  const generate = async (prompt) => {
+    updateLoading(true);
+    const result = await axios.get(`http://127.0.0.1:8000/?prompt=${prompt}`);
+    updateImage(result.data);
+    updateLoading(false);
+  };
+
+  return (
+    <ChakraProvider>
+      <Container>
+        <Heading>Text to Image Generator</Heading>
+        <Text marginBottom={"10px"}>
+        This is a React based application to generate image based on input text. Its free to use but before that run the api provided locally. API code is also given in the parent directory or you can click below to directly navigate there.{" "}<br/>
+          <Link href={""} bgColor={"blue"}>
+            ...API Link...
+          </Link>
+        </Text>
+
+        <Wrap marginBottom={"10px"}>
+          <Input
+            value={prompt}
+            onChange={(e) => updatePrompt(e.target.value)}
+            width={"350px"}
+          ></Input>
+          <Button onClick={(e) => generate(prompt)} colorScheme={"yellow"}>
+            Generate
+          </Button>
+        </Wrap>
+
+        {loading ? (
+          <Stack>
+            <SkeletonCircle />
+            <SkeletonText />
+          </Stack>
+        ) : image ? (
+          <Image src={`data:image/png;base64,${image}`} boxShadow="lg" />
+        ) : null}
+      </Container>
+    </ChakraProvider>
+  );
+};
+
+export default App;
